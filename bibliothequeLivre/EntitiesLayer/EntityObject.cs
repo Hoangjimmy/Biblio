bd3dd1b74905b41e57febfd54e0c6a00309a75f9
Terred <antoine.gueleraud@gmail.com>
2014-12-19 16:10:06 +0100

    Ajout des entites

---
+++ bibliothequeLivre/EntitiesLayer/EntityObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayer
+{
+    abstract class EntityObject
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public EntityObject()
+        {
+
+        }
+
+        public override bool Equals(Object o)
+        {
+            bool equal = false;
+            if (o.GetType() == typeof(EntityObject))
+                if (((EntityObject)o).Id == this.Id)
+                    equal = true;
+            return equal;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+    }
+}
---
+++ bibliothequeLivre/EntitiesLayer/Livre.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiesLayer
+{
+    class Livre : EntityObject
+    {
+        public String Auteur { get; set; }
+        public DateTime DateParution{ get; set; }
+        public String Editeur { get; set; }
+        public Genre Genre { get; set; }
+        public String ISBN { get; set; }
+        public int NombrePages { get; set; }
+        public String Titre { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder("Livre : ");
+            sb.Append(Auteur).Append(", ")
+                .Append(DateParution).Append(", ")
+                .Append(Editeur).Append(", ")
+                .Append(Genre).Append(", ")
+                .Append(NombrePages).Append(", ")
+                .Append(Titre);
+            return sb.ToString();
+        }
+
+        public Livre(String auteur, DateTime dateParution, String editeur, Genre genre, String isbn, int nombrePages, String titre)
+        {
+            Auteur = auteur;
+            DateParution = dateParution;
+            Editeur = editeur;
+            Genre = genre;
+            ISBN = isbn;
+            NombrePages = nombrePages;
+            Titre = titre;
+    }
+}
