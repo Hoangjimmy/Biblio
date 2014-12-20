0886bc37ef17b007356d64af9eaa6eb9a4d5d58c
Terred <antoine.gueleraud@gmail.com>
2014-12-20 16:09:14 +0100

    Rajout des methode de DalManager

---
+++ bibliothequeLivre/BusinessLayer/BiblioManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StubDataAccessLayer;
+
+namespace BusinessLayer
+{
+    public class BiblioManager
+    {
+        private DalManager dal = new DalManager();
+
+        static IList<String> listeDesEmpruntsEnCours()
+        {
+            List<String> resultat = new List<string>();
+
+            return resultat;
+        }
+
+        static IList<String> listeDesAuteursAvecPrixGoncourt()
+        {
+            List<String> resultat = new List<string>();
+
+            return resultat;
+        }
+
+        static IList<String> listeDesLivresAvecNoteSuperieurACinq()
+        {
+            List<String> resultat = new List<string>();
+
+            return resultat;
+        }
+
+        static IList<String> listeDesLivresAvecNoteSuperieurACinqEtAuteurAvecPrixGoncourt()
+        {
+            List<String> resultat = new List<string>();
+
+            return resultat;
+        }
+    }
+}
