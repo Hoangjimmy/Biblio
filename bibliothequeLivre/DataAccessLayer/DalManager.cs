330f93f08bf8eec067873c1e8eb6f719f5fb2ced
Hoangjimmy <yumiquet@gmail.com>
2015-02-12 23:21:04 +0100

    BDD
    Base de données

--- bibliothequeLivre/DataAccessLayer/DalManager.cs
+++ bibliothequeLivre/DataAccessLayer/DalManager.cs
@@ -95,9 +95,12 @@ namespace DataAccessLayer
             aut.DateNaissance = (DateTime)dr["DateNaissance"];
             return aut;
         }
-        public static Genre getGenreByID(int id) { 
-        
-        
+        public static Genre getGenreByID(int id)
+        {
+            DataTable dt = Datafill("SELECT Nom FROM Genres WHERE ID = " + id);
+            DataRow dr = dt.Rows[0];
+            Genre res = new Genre(id, dr["Nom"].ToString());
+            return res;
         }
         public static List<Auteur> getAuteurs() 
         {
@@ -127,7 +130,7 @@ namespace DataAccessLayer
                 n.Auteur = getAuteurById((int)dr["AuteurID"]);
                 n.DateParution = (DateTime)dr["DateParution"];
                 n.Editeur = dr["EditeurName"].ToString();
-               // n.Genre = dr["GenreID"].ToString();
+                n.Genre = getGenreByID((int)dr["GenreID"]);
                 n.ISBN = dr["ISBN"].ToString();
                 n.NombrePages = (int)dr["NombrePages"];
                 n.Note = (int)dr["Note"];
@@ -142,7 +145,7 @@ namespace DataAccessLayer
             DataTable dt = Datafill("SELECT * FROM Emprunts");
             foreach (DataRow dr in dt.Rows)
             {
-                Emprunt n = new Emprunt (
+                     Emprunt n = new Emprunt (
                     (DateTime)dr["DATE_DEBUT"],
                     (DateTime)dr["DATE_FIN"],
                     getEmprunteurByID((int)dr["ID_EMPRUNTEUR"]),
@@ -156,6 +159,164 @@ namespace DataAccessLayer
         {
             DataTable dt = Datafill("Select * FROM Collections");
             List<Collection> res = new List<Collection>();
+            foreach (DataRow dr in dt.Rows)
+            {
+                IList<Livre> listLivre = new List<Livre>();
+                DataTable dt2 = Datafill("SELECT LivresId FROM LivreCollection WHERE CollectionId =" +(int)dr["ID"]);
+                foreach(DataRow dr2 in dt2.Rows)
+                {
+                    Livre livre = getLivreByID((int)dr2["ID"]);
+                    listLivre.Add(livre);
+                }
+                Collection n = new Collection(listLivre);
+                res.Add(n);
+            }
+            return res;
+        }
+
+        public void AddModifyNewAuteur(Auteur aut)
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Jean\Downloads\EasyBorrowing.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand command = new SqlCommand ("UPDATE Auteurs SET ID = @id, Prenom = @prenom, Nom = @nom, DateNaissance = @datenaiss, DateDeces = @datedece, PrixGoncourt = @Prix, Sexe = @Sexe FROM (SELECT ID FROM AUTEUR WHERE ID = @id", connect);
+            command.Parameters.AddWithValue("@id", aut.Id);
+            command.Parameters.AddWithValue("@prenom", aut.Prenom);
+            command.Parameters.AddWithValue("@nom", aut.Nom);
+            command.Parameters.AddWithValue("@datenaiss", aut.DateNaissance);
+            command.Parameters.AddWithValue("@datedece", aut.DateMort);
+            command.Parameters.AddWithValue("@Prix", aut.PrixGoncourt);
+            command.Parameters.AddWithValue("@Sexe", aut.Sexe);
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+        }
+        public void DeleteAuteur(Auteur aut)
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Jean\Downloads\EasyBorrowing.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand command = new SqlCommand("DELETE FROM Auteurs WHERE ID = @id", connect);
+            command.Parameters.AddWithValue("@id", aut.Id);
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+        }
+        public void AddModifyCollection( Collection Collec)
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Jean\Downloads\EasyBorrowing.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand command = new SqlCommand("UPDATE Collections SET ID = @id,CollectionName = @cn, Description = @Descri  FROM (SELECT ID FROM Collections WHERE ID = @id", connect);
+            command.Parameters.AddWithValue("@id", Collec.Id);
+            command.Parameters.AddWithValue("@cn", Collec.Nom);
+            command.Parameters.AddWithValue("@Descri", Collec.Description);
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+        }
+        public void DeleteCollection(Collection Collec)
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Jean\Downloads\EasyBorrowing.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand command = new SqlCommand("DELETE FROM Collections WHERE ID = @id", connect);
+            command.Parameters.AddWithValue("@id", Collec.Id);
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+        }
+        public void AddModifyEmprunteur(Emprunteur emp)
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Jean\Downloads\EasyBorrowing.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand command = new SqlCommand("UPDATE Emprunteurs SET ID = @id, Prenom = @pre, Nom = @n, Sexe = @s, DateNaissance = @dn, Adresse = @addr, EMail = @em, Telephone = @tel  FROM (SELECT ID FROM Emprunteurs WHERE ID = @id", connect);
+            command.Parameters.AddWithValue("@id", emp.Id);
+            command.Parameters.AddWithValue("@n", emp.Nom);
+            command.Parameters.AddWithValue("@pre", emp.Prenom);
+            command.Parameters.AddWithValue("@s", emp.Sexe);
+            command.Parameters.AddWithValue("@dn", emp.DateNaissance);
+            command.Parameters.AddWithValue("@addr", emp.Adresse);
+            command.Parameters.AddWithValue("@em", emp.Email);
+            command.Parameters.AddWithValue("@tel", emp.Tel);
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+        }
+        public void DeleteEmprunteur(Emprunteur emp)
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Jean\Downloads\EasyBorrowing.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand command = new SqlCommand("DELETE FROM Emprunteurs WHERE ID = @id", connect);
+            command.Parameters.AddWithValue("@id", emp.Id);
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+        }
+        public void AddModifyEmprunt(Emprunt emp)
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Jean\Downloads\EasyBorrowing.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand command = new SqlCommand("UPDATE Emprunts SET ID = @id, ID_LIVRE = @idli, ID_EMPRUNTEUR = @idemp, DATE_DEBUT = @Date debut, DATE_FIN = @datefin, DATE_FIN_PREVUE = @datefinprevu  FROM (SELECT ID FROM Emprunts WHERE ID = @id", connect);            
+            command.Parameters.AddWithValue("@id", emp.Id);
+            command.Parameters.AddWithValue("@idli", emp.Livre.Id);
+            command.Parameters.AddWithValue("@idemp", emp.Emprunter.Id);
+            command.Parameters.AddWithValue("@Datedebut", emp.DateDebut);
+            command.Parameters.AddWithValue("@datefin", emp.DateFin);
+            command.Parameters.AddWithValue("@datefinprevu", emp.DateDebut.AddDays(14));
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+        }
+        public void DeleteEmprunt(Emprunt emp)
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Jean\Downloads\EasyBorrowing.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand command = new SqlCommand("DELETE FROM Emprunts WHERE ID = @id", connect);
+            command.Parameters.AddWithValue("@id", emp.Id);
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+        }
+
+        public void AddModifyGenre(Genre g)
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Jean\Downloads\EasyBorrowing.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand command = new SqlCommand("UPDATE Genres SET ID = @id, Nom = @n  FROM (SELECT ID FROM Genres WHERE ID = @id", connect);
+            command.Parameters.AddWithValue("@id",g.Id);
+            command.Parameters.AddWithValue("@n", g.Nom);
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+        }
+        public void DeleteGenre(Genre g)
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Jean\Downloads\EasyBorrowing.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand command = new SqlCommand("DELETE FROM Genres WHERE ID = @id", connect);
+            command.Parameters.AddWithValue("@id", g.Id);
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+        }
+        public void AddModifyLivre(Livre liv )
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Jean\Downloads\EasyBorrowing.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand command = new SqlCommand("UPDATE Livres SET ID = @id, AuteurID = @Aid, DateParution = @Dateparu, EditeurName = @en, GenreID = @gid, ISBN = @isbn, NombrePages = @nbpage, Note = @note, Titre = @titre  FROM (SELECT ID FROM Livres WHERE ID = @id", connect);
+            command.Parameters.AddWithValue("@id", liv.Id);
+            command.Parameters.AddWithValue("@Aid", liv.Auteur.Id);
+            command.Parameters.AddWithValue("@DateParu", liv.DateParution);
+            command.Parameters.AddWithValue("@en", liv.Editeur);
+            command.Parameters.AddWithValue("@gid", liv.Genre.Id);
+            command.Parameters.AddWithValue("@isbn", liv.ISBN);
+            command.Parameters.AddWithValue("@nbpage", liv.NombrePages);
+            command.Parameters.AddWithValue("@note", liv.Note);
+            command.Parameters.AddWithValue("@titre", liv.Titre);
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+        }
+        public void DeleteLivre(Livre liv)
+        {
+            SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Jean\Downloads\EasyBorrowing.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlCommand command = new SqlCommand("DELETE FROM Livres WHERE ID = @id", connect);
+            command.Parameters.AddWithValue("@id", liv.Id);
+            connect.Open();
+            command.ExecuteNonQuery();
+            connect.Close();
+        }
+
+        public Utilisateur GetUtilisateurByLogin(String login, String mdp)
+        {
+            Utilisateur user = new Utilisateur();
+
 
         }
     }
--- bibliothequeLivre/EntitiesLayer/Genre.cs
+++ bibliothequeLivre/EntitiesLayer/Genre.cs
@@ -8,22 +8,25 @@ namespace EntitiesLayer
 {
     public sealed class Genre
     {
-        private int ID;
+        public int Id{private set; get;}
+        public string Nom {private set; get;}
 
-        public static readonly Genre BandeDessinee = new Genre(0);
-        public static readonly Genre Biographie = new Genre(1);
-        public static readonly Genre Conte = new Genre(2);
-        public static readonly Genre Description = new Genre(3);
-        public static readonly Genre Fantastique = new Genre(4);
-        public static readonly Genre Horreur = new Genre(5);
-        public static readonly Genre Name = new Genre(6);
-        public static readonly Genre Nouvelle = new Genre(7);
-        public static readonly Genre Roman = new Genre(8);
-        public static readonly Genre ScienceFiction = new Genre(9);
 
-        private Genre(int ID)
+        public static readonly Genre BandeDessinee = new Genre(0,"Bande Dessinee");
+        public static readonly Genre Biographie = new Genre(1,"Biographie");
+        public static readonly Genre Conte = new Genre(2,"Conte");
+        public static readonly Genre Description = new Genre(3,"Description");
+        public static readonly Genre Fantastique = new Genre(4,"Fantastique");
+        public static readonly Genre Horreur = new Genre(5,"Horreur");
+        public static readonly Genre Name = new Genre(6, "Name");
+        public static readonly Genre Nouvelle = new Genre(7,"Nouvelle");
+        public static readonly Genre Roman = new Genre(8,"Roman");
+        public static readonly Genre ScienceFiction = new Genre(9,"Science Fiction");
+
+        private Genre(int id, string nom)
         {
-
+            this.Id = id;
+            this.Nom = nom;
         }
     }
 }
